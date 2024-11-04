56201d464899a573e85e6bf7a2ca90e85dc74c42
IgorAPSantos <igorppandre@hotmail.com>
2024-11-04 20:29:20 -0300

    criação pagina Signup

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,13 @@
+import { RegisterForm } from "../../components/User/RegisterForm"
+
+export const Signup = () => {
+    return (
+        <>
+            <RegisterForm />
+        </>
+    )
+}
+
+
+
+
