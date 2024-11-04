7d8fbcec43b427937c54bdffcd8179f244872740
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-04 19:00:38 -0300

    Rota feitas

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,3 +4,14 @@ const API = import.meta.env.API_URL;
 
 export const api = axios.create({
     baseURL: API,});
+
+
+export const getAllProducts = async () => {
+    const response = await api.get('/products');
+    return response.data;
+}
+
+export const getProductById = async (id) => {
+    const response = await api.get(`/products/${id}`);
+    return response.data;
+}
