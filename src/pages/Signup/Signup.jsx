ee0ac4318446f7dc59f00cbff542155afcd876cb
IgorAPSantos <igorppandre@hotmail.com>
2024-11-04 21:06:15 -0300

    add botão de ir para login na tela signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,9 +1,14 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 import { RegisterForm } from "../../components/User/RegisterForm"
 
 export const Signup = () => {
+    const history = useHistory()
+
     return (
         <>
             <RegisterForm />
+            <br /> <hr />
+            <button onClick={() => { history.push('/login') }} >Go To Login</button>
         </>
     )
 }
