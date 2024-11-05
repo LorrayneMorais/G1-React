89366ab4b8c67e2167530556ae165a2d1e0e6dba
IgorAPSantos <igorppandre@hotmail.com>
2024-11-05 13:18:08 -0300

    add userId no localStorage

--- src/components/User/LoginForm.jsx
+++ src/components/User/LoginForm.jsx
@@ -22,22 +22,21 @@ export const LoginForm = () => {
         const validPassword = await bcrypt.compare(password, user.password)
 
         if (validPassword) {
+            localStorage.setItem('userId', user.id)
             history.push('/home')
         } else {
             alert('Email or password invalid!')
         }
     }
     return (
-        <>
-            <form onSubmit={handleLoginSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '2vh' }}>
-                <label htmlFor="email">Email:
-                    <input required type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </label>
-                <label htmlFor="password">Password:
-                    <input required type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-                <button type="submit">Login</button>
-            </form>
-        </>
+        <form onSubmit={handleLoginSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '2vh' }}>
+            <label htmlFor="email">Email:
+                <input required type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label htmlFor="password">Password:
+                <input required type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <button type="submit">Login</button>
+        </form>
     )
 }
