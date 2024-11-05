e807089c1b9ab5f1bbbf303f0eace4099a50dbd9
IgorAPSantos <igorppandre@hotmail.com>
2024-11-05 17:18:13 -0300

    criacao rotas privadas

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import { Login } from './pages/Login/Login'
 import { Products } from './pages/Products/Products';
 import { Signup } from './pages/Signup/Signup'
 import { Cart } from './pages/Cart/Cart'
-import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import { SignUpContext } from './contexts/SignUpContext/SignUpContext';
+import { useContext } from 'react';
+import { NotLogged } from './pages/notLogged/notLogged';
 
-
+const PrivateRoute = ({ path, component }) => {
+  const { logged } = useContext(SignUpContext)
+  return (
+    logged ? <Route exact path={path} component={component} /> : <Redirect to='/notLogged' />
+  )
+}
 
 function App() {
   return (
@@ -13,8 +21,9 @@ function App() {
       <Switch>
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/login' component={Login} />
-        <Route exact path='/home' component={Products} />
-        <Route exact path='/cart' component={Cart} />
+        <Route exact path='/notLogged' component={NotLogged} />
+        <PrivateRoute exact path='/home' component={Products} />
+        <PrivateRoute exact path='/cart' component={Cart} />
       </Switch>
     </BrowserRouter>
   );
