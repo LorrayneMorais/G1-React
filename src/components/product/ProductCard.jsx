624de18e39f7e55452844e90bedf30d474e5a796
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-06 16:57:21 -0300

    Preço bonitos

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -5,6 +5,7 @@ import { MdAddShoppingCart } from "react-icons/md";
 import { ProductContext } from '../../contexts/CartContext/ProductContext';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { formatPrice } from '../../utils/PriceFormatter';
 
 export function ProductCard({ data }) {
     const { imgUrl, name, price, id } = data;
@@ -35,7 +36,7 @@ export function ProductCard({ data }) {
                 <img src={imgUrl} alt={name} className="card__image" />
             </Link>
             <div className="card-infos">
-                <h2 className="card__price">{price}</h2>
+                <h2 className="card__price">{formatPrice(price)}</h2>
                 <h2 className="card__title">{name}</h2>
             </div>
             <button type="button" className="card__button">
