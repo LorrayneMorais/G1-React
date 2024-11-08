4f61563da1b105992afb24e91132c10b5e9d34db
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-08 14:25:18 -0300

    menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { CartButton } from '../Cart/CartButton/CartButton';
 import './Header.css';
 import { SearchBar } from '../SearchBar/SearchBar';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
     return (
         <header className="container-header">
             <div className="header-search">
-                <div>
-                    navegação
+                <div className="header-categoria">
+                    <RxHamburgerMenu />
                 </div>
                 <div>
                     <SearchBar />
