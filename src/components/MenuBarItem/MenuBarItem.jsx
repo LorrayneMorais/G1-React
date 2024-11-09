ceee5e1d1204a2b84e5eba8e63c709cb5dd8490e
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-08 22:38:15 -0300

    Update MenuBarItem.jsx

--- src/components/MenuBarItem/MenuBarItem.jsx
+++ src/components/MenuBarItem/MenuBarItem.jsx
@@ -6,14 +6,16 @@ import { FaSearch } from "react-icons/fa";
 import './MenuBarItem.css';
 
 
-export function MenuBarItem({categories}) {
+export function MenuBarItem({categories, handleCategoryFilter, filter}) {
+    // MenuBar fazer funcao handleCategroyFilter (array de categorias)
+    //Vou passar as handleCategoryFilter como props
 
     const renderCategories = () => {
         return categories.map((category, index) => {
             return (
                 <div className="menu-categories" key={index}>
                     <label htmlFor={category}>{category}</label>
-                    <input type="checkbox" id={category}/>
+                    <input type="checkbox" id={category} onChange={()=> {handleCategoryFilter}} value={filter}/>
                 </div>
             )
         })
@@ -31,44 +33,7 @@ export function MenuBarItem({categories}) {
                 </div>
                 <br />
                 <div>
-
                     {renderCategories()}
-                    {/* <div className="menu-categories">
-                        <label htmlFor="acao">Ação</label>
-                        <input type="checkbox" id="acao"/>
-                    </div> */}
-                    {/* <div className="menu-categories">
-                        <label htmlFor="suspense">Suspense</label>
-                        <input type="checkbox" id="suspense"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="terror">Terror</label>
-                        <input type="checkbox" id="terror"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="romance">Romance</label>
-                        <input type="checkbox" id="romance"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="ficcao-cientifica">Ficção-Cientifica</label>
-                        <input type="checkbox" id="ficcao-cientifica"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="misterio">Mistério</label>
-                        <input type="checkbox" id="misterio"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="financas">Finanças</label>
-                        <input type="checkbox" id="financas"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="programacao">Programação</label>
-                        <input type="checkbox" id="programacao"/>
-                    </div>
-                    <div className="menu-categories">
-                        <label htmlFor="japones">Japonês</label>
-                        <input type="checkbox" id="japones"/>
-                    </div> */}
                 </div>
             </section>
         </>
