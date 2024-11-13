8f47fe63e2c07acd04135d4e36749563428ae8a4
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-13 15:22:58 -0300

    validação do carrinho

--- src/components/Cart/CartButton/CartButton.jsx
+++ src/components/Cart/CartButton/CartButton.jsx
@@ -5,10 +5,17 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { ProductContext } from "../../../contexts/CartContext/ProductContext";
 
 
+
 export function CartButton() {
     const {cart, isCartVisible, setIsCartVisible} = useContext(ProductContext);
+    const handleCart = () => {
+        if(cart.length == 0) {
+            alert("Adcione um produto ao carrinho");
+        }
+        setIsCartVisible(!isCartVisible);
+    }
     return (
-        <button type="button" onClick={() => setIsCartVisible(!isCartVisible)} className="cart__button">
+        <button type="button" onClick={handleCart} className="cart__button">
             <AiOutlineShoppingCart/>
             {cart.map((item) => Number(item.quantity)).reduce((a, b) => a + b, 0) > 0 && <span className="cart-status"> {cart.map((item) => Number(item.quantity)).reduce((a, b) => a + b, 0)}</span>}
         </button>
