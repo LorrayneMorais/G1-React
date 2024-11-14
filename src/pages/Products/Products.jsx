e6822dcbdafdd11aa11f9969252b873d07784b12
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-14 16:25:00 -0300

    Update Products.jsx

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -5,7 +5,7 @@ import { getAllProducts } from '../../api/api';
 import { ProductContext } from '../../contexts/CartContext/ProductContext';
 import { Loading } from '../../components/Loading/Loading';
 import Header from '../../components/header/Header'
-import  Footer  from '../../components/Footer/Footer.jsx';
+import  Footer  from '../../components/Footer/Footer';
 import { Cart } from '../Cart/Cart';
 import { MenuBar } from '../MenuBar/MenuBar';
 // import { BsHandIndexThumb } from 'react-icons/bs';
